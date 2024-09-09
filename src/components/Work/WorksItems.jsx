90026cddb3a2437fc726dfeca9eecbf5fc128983
Awais Ahmad <itxawais211@gmail.com>
2024-09-09 01:23:24 -0700

    Portfolio section completed

---
+++ src/components/Work/WorksItems.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BiRightArrowAlt } from 'react-icons/bi'
+
+const WorksItems = ({item}) => {
+  return (
+    <div className='work__card' key={item.id}>
+      <img src={item.image} alt="Image" className='work__img'/>
+      <h3 className="work__title">{item.title}</h3>
+      <a href="#" className="work__button">
+        Demo <BiRightArrowAlt className='work__button-icon'/>
+      </a>
+    </div>
+  )
+}
+
+export default WorksItems
